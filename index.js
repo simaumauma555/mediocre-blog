95fbbff36284204c2acde39b84323f770c492712
simaumauma555 <simaumauma555@gmail.com>
2020-08-22 11:00:22 +0100

    Update

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ let blogTitle4=document.getElementById("blog_title4");
 let blogTitle5=document.getElementById("blog_title5");
 let blogTitle6=document.getElementById("blog_title6");
 let blogTitle7=document.getElementById("blog_title7");
+let blogTitle8=document.getElementById("blog_title8");
 
 let englishLearningButton =document.getElementById("english_learning");
 englishLearningButton.onclick=function(){
@@ -18,6 +19,7 @@ englishLearningButton.onclick=function(){
     blogTitle5.href="English_learning/Nuance.html";
     blogTitle6.href="English_learning/Improve_English_speaking_skill_with_conjunction.html";
     blogTitle7.href="English_learning/How_to_improve_english_speaking_for_beginner.html";
+    blogTitle8.href="English_learning/How_to_improve_zatsudan.html";
     blogTitle1.innerText="英語の聞き流しはリスニング力向上に効果的か？";
     blogTitle2.innerText="英語を聞き取れるようになる方法";
     blogTitle3.innerText="英語のリスニング力を劇的に向上には？";
@@ -25,6 +27,7 @@ englishLearningButton.onclick=function(){
     blogTitle5.innerText="英単語をニュアンス（本質的意味）で覚えるべき理由";
     blogTitle6.innerText="接続表現を極めて英語スピーキングを上達しよう";
     blogTitle7.innerText="英語スピーキング力の上達方法（初～中級者）";
+    blogTitle8.innerText="英語での雑談を劇的に上達する方法";
     titleImage.src="pictures/uk_image2.JPG";
     explanation.innerText="30歳手前で突然海外関連部門に配属され、そこから慌てて英語を勉強し始め、なんとかビジネスレベル（TOEIC: 980点）の英語を獲得しました。英語を勉強されている方にその経験を共有し、微力ながら参考になる情報を提供したいと思います。";
 };
@@ -33,19 +36,27 @@ let UKLifeButton =document.getElementById("UK_life");
 UKLifeButton.onclick=function(){
     title.innerText="イギリスの生活";
     titleImage.src="pictures/uk_image1.JPG";
+    let parent=document.getElementById("landing_page_main");
+    parent.removeChild(parent.children[9]);
+    parent.removeChild(parent.children[8]);
+    parent.removeChild(parent.children[7]);
+    parent.removeChild(parent.children[6]);
+    parent.removeChild(parent.children[5]);
     blogTitle1.href="UK_life/UK_traffic_rule/traffic_speed.html";
     blogTitle2.href="UK_life/UK_traffic_rule/traffic_differences.html";
-    blogTitle3.href="";
-    blogTitle4.href="";
-    blogTitle5.href="";
-    blogTitle6.href="";
-    blogTitle7.href="";
-    blogTitle1.innerText="【完全攻略】イギリスの制限速度";
-    blogTitle2.innerText="【徹底解説】安心してイギリスで運転するために必要な事前知識";
-    blogTitle3.innerHTML="準備中";
-    blogTitle4.innerHTML="準備中";
-    blogTitle5.innerHTML="準備中";
-    blogTitle6.innerText="準備中";
-    blogTitle7.innerText="準備中";
+    blogTitle3.href="UK_life/UK_traffic_rule/traffic_differences.html2";
+    // blogTitle4.href="";
+    // blogTitle5.href="";
+    // blogTitle6.href="";
+    // blogTitle7.href="";
+    // blogTitle8.href="";
+    blogTitle1.innerText="イギリスの制限速度";
+    blogTitle2.innerText="安心してイギリスで運転するために必要な事前知識";
+    blogTitle3.innerText="安心してイギリスで運転するために必要な事前知識２";
+    // blogTitle4.innerHTML="";
+    // blogTitle5.innerHTML="";
+    // blogTitle6.innerHTML="";
+    // blogTitle7.innerHTML="";
+    // blogTitle8.innerHTML="";
     explanation.innerText="海外駐在の機会をいただきイギリスに生活しております（2020年時点）。今後イギリスに住まれる方や旅行でイギリスに来られる方に向けて、有用な情報を提供していきたいと考えています。" 
 };
